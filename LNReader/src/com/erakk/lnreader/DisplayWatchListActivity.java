305a85f86f8dd8118381206a26c22dc254410915
Calvin Gonçalves de Aquino <calving.aquino@gmail.com>
2012-08-12 19:49:39 -0300

    Updated menus
    Watchlist can now go directly to settings
    Main activity has "Last Novel Read" menu implemented
    now it needs real code (we have to be bale to open a chapter first =D )

--- LNReader/src/com/erakk/lnreader/DisplayWatchListActivity.java
+++ LNReader/src/com/erakk/lnreader/DisplayWatchListActivity.java
@@ -2,6 +2,7 @@ package com.erakk.lnreader;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
@@ -25,6 +26,10 @@ public class DisplayWatchListActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+        	case R.id.menu_settings:
+        		Intent launchNewIntent = new Intent(this, DisplaySettingsActivity.class);
+        		startActivity(launchNewIntent);
+        		return true;
             case android.R.id.home:
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
--- LNReader/src/com/erakk/lnreader/MainActivity.java
+++ LNReader/src/com/erakk/lnreader/MainActivity.java
@@ -3,8 +3,11 @@ package com.erakk.lnreader;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +21,22 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        	case R.id.last_novel:
+        		Toast last = Toast.makeText(this, "Last Novel read option not implemented yet.", Toast.LENGTH_SHORT);
+        		last.show();
+        		//Intent launchNewIntent = new Intent(this, DisplaySettingsActivity.class);
+        		//startActivity(launchNewIntent);
+        		return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
     
     public void openNovelList(View view) {
