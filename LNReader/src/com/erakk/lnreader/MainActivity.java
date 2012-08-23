0d2b4c136899c95d98e56713d0f6b9396f687da1
Nandaka <nchek2000@gmail.com>
2012-08-23 21:42:56 +0800

    Testing using theme for main

--- LNReader/src/com/erakk/lnreader/MainActivity.java
+++ LNReader/src/com/erakk/lnreader/MainActivity.java
@@ -1,6 +1,8 @@
 package com.erakk.lnreader;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -15,9 +17,21 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    @Override
+    @SuppressLint("NewApi")
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(false);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+    	boolean invertColors = sharedPrefs.getBoolean("invert_colors", false);
+    	if(invertColors) {
+    		setTheme(R.style.AppTheme);
+    		Toast.makeText(this, "Light.", Toast.LENGTH_SHORT).show();
+    	}
+    	else {
+    		setTheme(R.style.DarkTheme);
+    		Toast.makeText(this, "Dark.", Toast.LENGTH_SHORT).show();
+    	}
         setContentView(R.layout.activity_main);
     }
 
@@ -27,14 +41,16 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    @Override
+    @SuppressLint("NewApi")
+	@Override
     protected void onResume() {
         super.onResume();
         // The activity has become visible (it is now "resumed").
-        updateViewColor();
+        //updateViewColor();
     }
     
-    @Override
+    @SuppressLint("NewApi")
+	@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
         	case R.id.last_novel:
@@ -49,7 +65,8 @@ public class MainActivity extends Activity {
         	case R.id.invert_colors:
     			
         		toggleColorPref();
-        		updateViewColor();
+        		//updateViewColor();
+        		recreate();
     			
     			Toast.makeText(getApplicationContext(), "Colors inverted", Toast.LENGTH_SHORT).show();
     			return true;
@@ -72,40 +89,40 @@ public class MainActivity extends Activity {
     	editor.commit();
     	//Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
-    
-    private void updateViewColor() {
-    	SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-    	boolean invertColors = sharedPrefs.getBoolean("invert_colors", false);
-    	
-    	// Views to be changed
-        View MainView = findViewById(R.id.main_screen);
-        Button Button1 = (Button)findViewById(R.id.button1);
-        Button Button2 = (Button)findViewById(R.id.button2);
-        Button Button3 = (Button)findViewById(R.id.button3);
-        MainView.invalidate();
-        
-        // it is considered white background and black text to be the standard
-        // so we change to black background and white text if true
-        if (invertColors == true) {
-        	MainView.setBackgroundColor(Color.BLACK);
-        	Button1.setTextColor(Color.WHITE);
-        	Button1.setBackgroundColor(Color.DKGRAY);
-        	Button2.setTextColor(Color.WHITE);
-        	Button2.setBackgroundColor(Color.DKGRAY);
-        	Button3.setTextColor(Color.WHITE);
-        	Button3.setBackgroundColor(Color.DKGRAY);
-        }
-        else {
-        	MainView.setBackgroundColor(Color.WHITE);
-        	Button1.setTextColor(Color.BLACK);
-        	Button1.setBackgroundColor(Color.LTGRAY);
-        	Button2.setTextColor(Color.BLACK);
-        	Button2.setBackgroundColor(Color.LTGRAY);
-        	Button3.setTextColor(Color.BLACK);
-        	Button3.setBackgroundColor(Color.LTGRAY);
-        }
-    }
-    
+//    
+//    private void updateViewColor() {
+//    	SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+//    	boolean invertColors = sharedPrefs.getBoolean("invert_colors", false);
+//    	
+//    	// Views to be changed
+//        View MainView = findViewById(R.id.main_screen);
+//        Button Button1 = (Button)findViewById(R.id.button1);
+//        Button Button2 = (Button)findViewById(R.id.button2);
+//        Button Button3 = (Button)findViewById(R.id.button3);
+//        MainView.invalidate();
+//        
+//        // it is considered white background and black text to be the standard
+//        // so we change to black background and white text if true
+//        if (invertColors == true) {
+//        	MainView.setBackgroundColor(Color.BLACK);
+//        	Button1.setTextColor(Color.WHITE);
+//        	Button1.setBackgroundColor(Color.DKGRAY);
+//        	Button2.setTextColor(Color.WHITE);
+//        	Button2.setBackgroundColor(Color.DKGRAY);
+//        	Button3.setTextColor(Color.WHITE);
+//        	Button3.setBackgroundColor(Color.DKGRAY);
+//        }
+//        else {
+//        	MainView.setBackgroundColor(Color.WHITE);
+//        	Button1.setTextColor(Color.BLACK);
+//        	Button1.setBackgroundColor(Color.LTGRAY);
+//        	Button2.setTextColor(Color.BLACK);
+//        	Button2.setBackgroundColor(Color.LTGRAY);
+//        	Button3.setTextColor(Color.BLACK);
+//        	Button3.setBackgroundColor(Color.LTGRAY);
+//        }
+//    }
+//    
     public void openNovelList(View view) {
     	Intent intent = new Intent(this, DisplayLightNovelListActivity.class);
     	intent.putExtra(Constants.EXTRA_ONLY_WATCHED, false);
