066a5c781f196f165db35509bb617306e53636dc
Nandaka <nchek2000@gmail.com>
2012-12-01 17:47:01 +0800

    Fix issue #17, update APK

--- LNReader/src/com/erakk/lnreader/helper/BakaTsukiWebChromeClient.java
+++ LNReader/src/com/erakk/lnreader/helper/BakaTsukiWebChromeClient.java
@@ -27,17 +27,22 @@ public class BakaTsukiWebChromeClient extends WebChromeClient {
 	public boolean onConsoleMessage (ConsoleMessage consoleMessage) {
 		if(consoleMessage.message().startsWith(HIGHLIGHT_EVENT)) {
 			String data[] = consoleMessage.message().split(":");
-			BookmarkModel bookmark = new BookmarkModel();
-			bookmark.setPage(caller.content.getPage());
-			bookmark.setpIndex(Integer.parseInt(data[1]));
-			if(data[2].equalsIgnoreCase(ADD)) {
-				bookmark.setExcerpt(data[3]);
-				NovelsDao.getInstance(caller).addBookmark(bookmark);
+			try{
+				int pIndex = Integer.parseInt(data[1]);
+				BookmarkModel bookmark = new BookmarkModel();
+				bookmark.setPage(caller.content.getPage());
+				bookmark.setpIndex(pIndex);
+				if(data[2].equalsIgnoreCase(ADD)) {
+					bookmark.setExcerpt(data[3]);
+					NovelsDao.getInstance(caller).addBookmark(bookmark);
+				}
+				else if(data[2].equalsIgnoreCase(REMOVE)) {
+					NovelsDao.getInstance(caller).deleteBookmark(bookmark);
+				}
+				caller.refreshBookmarkData();
+			}catch(NumberFormatException ex) {
+				Log.e(TAG, "Error when parsing pIndex: " + ex.getMessage(), ex);
 			}
-			else if(data[2].equalsIgnoreCase(REMOVE)) {
-				NovelsDao.getInstance(caller).deleteBookmark(bookmark);
-			}
-			caller.refreshBookmarkData();
 		}
 		else if (consoleMessage.message().startsWith(SCROLL_EVENT)) {
 			String data[] = consoleMessage.message().split(":");
