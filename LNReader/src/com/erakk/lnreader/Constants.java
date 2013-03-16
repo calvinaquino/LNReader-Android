a8fa96840fb30d76b977b98c82e921c973605877
Nandaka <nchek2000@gmail.com>
2013-03-16 15:02:20 +0800

    Add preferences for ui selection

--- LNReader/src/com/erakk/lnreader/Constants.java
+++ LNReader/src/com/erakk/lnreader/Constants.java
@@ -69,6 +69,7 @@ public class Constants {
 	public static final String PREF_USE_CUSTOM_CSS = "use_custom_css";
 	public static final String PREF_CUSTOM_CSS_PATH = "custom_css_path";
 	public static final String PREF_MARGINS = "margin_space";
+	public static final String PREF_UI_SELECTION = "ui_selection";
 	
 	public static final float DISPLAY_SCALE = LNReaderApplication.getInstance().getResources().getDisplayMetrics().density ;
 
--- LNReader/src/com/erakk/lnreader/activity/MainActivity.java
+++ LNReader/src/com/erakk/lnreader/activity/MainActivity.java
@@ -25,186 +25,164 @@ import com.erakk.lnreader.UIHelper;
 public class MainActivity extends SherlockActivity {
 	private static final String TAG = MainActivity.class.toString();
 	private boolean isInverted;
-	
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB )
-            UIHelper.SetTheme(this, R.layout.activity_main);
-		else{
-	        UIHelper.SetTheme(this, R.layout.activity_main_no_tab);
-		}
-        UIHelper.SetActionBarDisplayHomeAsUp(this, false);
-        isInverted = getColorPreferences();
-        setIconColor();
-        
-        if (isFirstRun()) {
-        	//Show copyrights
 
-        	new AlertDialog.Builder(this).setTitle("Terms of Use").setMessage("Before using this application, keep in mind that we, the developers of BakaTsuki EX, are not responsible for the content displayed by the application in any way. Therefore, you must read and agree to the TLG Translation Common Agreement of Baka-Tsuki.org:\n\n" + getString(R.string.bakatsuki_copyrights) + "\n\nBy clicking \"I Agree\" below, you confirm that you have read the TLG Translation Common Agreement in it's entirety.").setPositiveButton("I Agree", new OnClickListener() {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+			UIHelper.SetTheme(this, R.layout.activity_main);
+		else {
+			UIHelper.SetTheme(this, R.layout.activity_main_no_tab);
+		}
+		UIHelper.SetActionBarDisplayHomeAsUp(this, false);
+		isInverted = getColorPreferences();
+		setIconColor();
+
+		if (isFirstRun()) {
+			// Show copyrights
+
+			new AlertDialog.Builder(this).setTitle("Terms of Use").setMessage("Before using this application, keep in mind that we, the developers of BakaTsuki EX, are not responsible for the content displayed by the application in any way. Therefore, you must read and agree to the TLG Translation Common Agreement of Baka-Tsuki.org:\n\n" + getString(R.string.bakatsuki_copyrights) + "\n\nBy clicking \"I Agree\" below, you confirm that you have read the TLG Translation Common Agreement in it's entirety.").setPositiveButton("I Agree", new OnClickListener() {
 
 				public void onClick(DialogInterface dialog, int which) {
-		        	setFirstRun();
+					setFirstRun();
 				}
 			}).setNegativeButton("Exit App", new OnClickListener() {
 				public void onClick(DialogInterface dialog, int which) {
 					finish();
 				}
 			}).show();
-        }
-        Log.d(TAG, "Main created.");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getSupportMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
-	@Override
-    protected void onRestart() {
-        super.onRestart();
-        if(isInverted != getColorPreferences()) {
-        	UIHelper.Recreate(this);
-        	setIconColor();
-        }
-    }
-	
-	@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-        	case R.id.invert_colors:    			
-        		UIHelper.ToggleColorPref(this);
-        		UIHelper.Recreate(this);
-        		setIconColor();
-    			return true;
-//        	case R.id.menu_search:    			
-//        		Intent intent = new Intent(this, DisplaySearchActivity.class);
-//            	startActivity(intent);  			
-//    			return true;
-//        	case R.id.menu_bookmarks:
-//        		Intent bookmarkIntent = new Intent(this, DisplayBookmarkActivity.class);
-//            	startActivity(bookmarkIntent);
-//    			return true;   
-//        	case R.id.menu_settings:
-//        		Intent settingsIntent = new Intent(this, DisplaySettingsActivity.class);
-//            	startActivity(settingsIntent);
-//    			return true; 
-//        	case R.id.menu_downloads:
-//        		Intent downloadsIntent = new Intent(this, DownloadListActivity.class);
-//            	startActivity(downloadsIntent);;
-//    			return true; 
-//        	case R.id.menu_update_history:
-//        		Intent updateHistoryIntent  = new Intent(this, UpdateHistoryActivity.class);
-//            	startActivity(updateHistoryIntent);;
-//    			return true;
-            case android.R.id.home:
-                finish();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-	
-    public void openNovelList(View view) {
-//    	Intent intent = new Intent(this, DisplayLightNovelListActivity.class);
-    	Intent intent = new Intent(this, DisplayNovelPagerActivity.class);
-    	intent.putExtra(Constants.EXTRA_ONLY_WATCHED, false);
-    	startActivity(intent);
-    }
-    public void openNovelListNoTab(View view) {
-    	Intent intent = new Intent(this, DisplayLightNovelListActivity.class);
-    	intent.putExtra(Constants.EXTRA_ONLY_WATCHED, false);
-    	startActivity(intent);
-    }
-    
-    public void openTeaserList(View view) {
-    	Intent intent = new Intent(this, DisplayTeaserListActivity.class);
-    	startActivity(intent);
-    }
-    
-    public void openOriginalsList(View view) {
-    	Intent intent = new Intent(this, DisplayOriginalListActivity.class);
-    	startActivity(intent);
-    }
-    public void openDownloadsList(View view) {
-    	Intent intent = new Intent(this, DownloadListActivity.class);
-    	startActivity(intent);
-    }
-    public void openUpdatesList(View view) {
-    	Intent intent = new Intent(this, UpdateHistoryActivity.class);
-    	startActivity(intent);
-    }
-    public void openBookmarks(View view) {
-    	Intent bookmarkIntent = new Intent(this, DisplayBookmarkActivity.class);
-    	startActivity(bookmarkIntent);
-    }
-    public void openSearch(View view) {
-    	Intent intent = new Intent(this, DisplaySearchActivity.class);
-    	startActivity(intent); 
-    }
-    
-    public void openWatchList(View view) {
-    	Intent intent = new Intent(this, DisplayLightNovelListActivity.class);
-    	intent.putExtra(Constants.EXTRA_ONLY_WATCHED, true);
-    	startActivity(intent);
-    }
-    
-    public void openSettings(View view) {
-    	Intent intent = new Intent(this, DisplaySettingsActivity.class);
-    	startActivity(intent);
-//    	FOR TESTING
-//    	resetFirstRun();
-    }
-    
-    public void jumpLastRead(View view) {
-		String lastReadPage = PreferenceManager.getDefaultSharedPreferences(this).getString(Constants.PREF_LAST_READ, "");
-		if(lastReadPage.length() > 0) {
-			Intent intent = new Intent(getApplicationContext(), DisplayLightNovelContentActivity.class);
-	        intent.putExtra(Constants.EXTRA_PAGE, lastReadPage);
-	        startActivity(intent);
 		}
-		else{
+		Log.d(TAG, "Main created.");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		if (isInverted != getColorPreferences()) {
+			UIHelper.Recreate(this);
+			setIconColor();
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.invert_colors:
+			UIHelper.ToggleColorPref(this);
+			UIHelper.Recreate(this);
+			setIconColor();
+			return true;
+		case android.R.id.home:
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void openNovelList(View view) {
+		String ui = PreferenceManager.getDefaultSharedPreferences(this).getString(Constants.PREF_UI_SELECTION, "0");
+		if(ui.equalsIgnoreCase("0")) {
+			Intent intent = new Intent(this, DisplayNovelPagerActivity.class);
+			intent.putExtra(Constants.EXTRA_ONLY_WATCHED, false);
+			startActivity(intent);
+		}
+		else if (ui.equalsIgnoreCase("1")) {
+			Intent intent = new Intent(this, TestDisplayNovelActivity.class);
+			intent.putExtra(Constants.EXTRA_ONLY_WATCHED, false);
+			startActivity(intent);
+		}
+		else if (ui.equalsIgnoreCase("2")) {
+			Intent intent = new Intent(this, TestDisplayNovelActivityTwo.class);
+			intent.putExtra(Constants.EXTRA_ONLY_WATCHED, false);
+			startActivity(intent);
+		}
+	}
+
+	public void openNovelListNoTab(View view) {
+		Intent intent = new Intent(this, DisplayLightNovelListActivity.class);
+		intent.putExtra(Constants.EXTRA_ONLY_WATCHED, false);
+		startActivity(intent);
+	}
+
+	public void openTeaserList(View view) {
+		Intent intent = new Intent(this, DisplayTeaserListActivity.class);
+		startActivity(intent);
+	}
+
+	public void openOriginalsList(View view) {
+		Intent intent = new Intent(this, DisplayOriginalListActivity.class);
+		startActivity(intent);
+	}
+
+	public void openDownloadsList(View view) {
+		Intent intent = new Intent(this, DownloadListActivity.class);
+		startActivity(intent);
+	}
+
+	public void openUpdatesList(View view) {
+		Intent intent = new Intent(this, UpdateHistoryActivity.class);
+		startActivity(intent);
+	}
+
+	public void openBookmarks(View view) {
+		Intent bookmarkIntent = new Intent(this, DisplayBookmarkActivity.class);
+		startActivity(bookmarkIntent);
+	}
+
+	public void openSearch(View view) {
+		Intent intent = new Intent(this, DisplaySearchActivity.class);
+		startActivity(intent);
+	}
+
+	public void openWatchList(View view) {
+		Intent intent = new Intent(this, DisplayLightNovelListActivity.class);
+		intent.putExtra(Constants.EXTRA_ONLY_WATCHED, true);
+		startActivity(intent);
+	}
+
+	public void openSettings(View view) {
+		Intent intent = new Intent(this, DisplaySettingsActivity.class);
+		startActivity(intent);
+		// FOR TESTING
+		// resetFirstRun();
+	}
+
+	public void jumpLastRead(View view) {
+		String lastReadPage = PreferenceManager.getDefaultSharedPreferences(this).getString(Constants.PREF_LAST_READ, "");
+		if (lastReadPage.length() > 0) {
+			Intent intent = new Intent(getApplicationContext(), DisplayLightNovelContentActivity.class);
+			intent.putExtra(Constants.EXTRA_PAGE, lastReadPage);
+			startActivity(intent);
+		} else {
 			Toast.makeText(this, "You have no novel to resume reading.", Toast.LENGTH_SHORT).show();
 		}
-    }
-    
-    // This button is only a testing button (REMOVE ON FINAL)
-    public void gotoTestRoom(View view) {
-    	Intent intent = new Intent(this, TestDisplayNovelActivity.class);
-    	intent.putExtra(Constants.EXTRA_ONLY_WATCHED, false);
-    	startActivity(intent);
-    }
-    
-    // This button is only a testing button (REMOVE ON FINAL)
-    public void gotoTestRoom2(View view) {
-    	Intent intent = new Intent(this, TestDisplayNovelActivityTwo.class);
-    	intent.putExtra(Constants.EXTRA_ONLY_WATCHED, false);
-    	startActivity(intent);
-    }
-    
-    
-//    public void openDownloads(View view) {
-//    	Intent intent = new Intent(this, DownloadListActivity.class);
-//    	startActivity(intent);
-//    }
-
-    private void setIconColor() {
-    	LinearLayout rightMenu = (LinearLayout) findViewById(R.id.menu_right);
-    	int childCount = rightMenu.getChildCount();
-    	for(int i = 0; i < childCount; ++i) {
-    		ImageButton btn = (ImageButton) rightMenu.getChildAt(i);
-    		btn.setImageDrawable(UIHelper.setColorFilter(btn.getDrawable()));
-    	}
-    }
-	private boolean getColorPreferences(){
-    	return PreferenceManager.getDefaultSharedPreferences(this).getBoolean(Constants.PREF_INVERT_COLOR, true);
 	}
+	private void setIconColor() {
+		LinearLayout rightMenu = (LinearLayout) findViewById(R.id.menu_right);
+		int childCount = rightMenu.getChildCount();
+		for (int i = 0; i < childCount; ++i) {
+			ImageButton btn = (ImageButton) rightMenu.getChildAt(i);
+			btn.setImageDrawable(UIHelper.setColorFilter(btn.getDrawable()));
+		}
+	}
+
+	private boolean getColorPreferences() {
+		return PreferenceManager.getDefaultSharedPreferences(this).getBoolean(Constants.PREF_INVERT_COLOR, true);
+	}
+
 	private boolean isFirstRun() {
 		return PreferenceManager.getDefaultSharedPreferences(this).getBoolean(Constants.PREF_FIRST_RUN, true);
 	}
+
 	private void setFirstRun() {
 		SharedPreferences.Editor edit = PreferenceManager.getDefaultSharedPreferences(this).edit();
-	    edit.putBoolean(Constants.PREF_FIRST_RUN, false);
-	    edit.commit();
+		edit.putBoolean(Constants.PREF_FIRST_RUN, false);
+		edit.commit();
 	}
 }
