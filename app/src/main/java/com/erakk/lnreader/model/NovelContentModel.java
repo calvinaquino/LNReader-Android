4952b834f6de88cd7c26f27561d1dad95c6da400
Nandaka <nchek2000@gmail.com>
2015-03-24 19:56:34 +0800

    Try to fix #177

--- app/src/main/java/com/erakk/lnreader/model/NovelContentModel.java
+++ app/src/main/java/com/erakk/lnreader/model/NovelContentModel.java
@@ -8,116 +8,142 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class NovelContentModel {
-	private static final String TAG = NovelContentModel.class.toString();
-	private int id = -1;
-	private String content;
-	private String page;
-	private PageModel pageModel;
+    private static final String TAG = NovelContentModel.class.toString();
+    private int id = -1;
+    private String content;
+    private String page;
+    private PageModel pageModel;
 
-	private int lastXScroll;
-	private int lastYScroll;
-	private double lastZoom;
+    private int lastXScroll;
+    private int lastYScroll;
+    private double lastZoom;
 
-	private Date lastUpdate;
-	private Date lastCheck;
+    private Date lastUpdate;
+    private Date lastCheck;
 
-	private boolean isUpdatingFromInternet;
+    private boolean isUpdatingFromInternet;
 
-	private ArrayList<ImageModel> images;
+    private ArrayList<ImageModel> images;
 
-	private ArrayList<BookmarkModel> bookmarks;
+    private ArrayList<BookmarkModel> bookmarks;
 
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getPage() {
-		return page;
-	}
-	public void setPage(String page) {
-		this.page = page;
-	}
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
     public void refreshPageModel() throws Exception {
-        if(this.pageModel == null) {
+        // try to disable the checking, may hit performance issue?
+        // refer to https://github.com/calvinaquino/LNReader-Android/issues/177
+        //if(this.pageModel == null) {
+        NovelsDao dao = NovelsDao.getInstance();
+        PageModel tempPage = new PageModel();
+        tempPage.setPage(this.page);
+        this.pageModel = dao.getPageModel(tempPage, null);
+        //}
+    }
+
+    public PageModel getPageModel() throws Exception {
+        if (this.pageModel == null) {
             NovelsDao dao = NovelsDao.getInstance();
             PageModel tempPage = new PageModel();
             tempPage.setPage(this.page);
             this.pageModel = dao.getPageModel(tempPage, null);
         }
+        return pageModel;
     }
-	public PageModel getPageModel() throws Exception {
-		if(this.pageModel == null) {
-			NovelsDao dao = NovelsDao.getInstance();
-			PageModel tempPage = new PageModel();
-			tempPage.setPage(this.page);
-			this.pageModel = dao.getPageModel(tempPage, null);
-		}
-		return pageModel;
-	}
-	public void setPageModel(PageModel pageModel) {
-		this.pageModel = pageModel;
-	}
-	public int getLastXScroll() {
-		return lastXScroll;
-	}
-	public void setLastXScroll(int lastXScroll) {
-		this.lastXScroll = lastXScroll;
-	}
-	public int getLastYScroll() {
-		return lastYScroll;
-	}
-	public void setLastYScroll(int lastYScroll) {
-		this.lastYScroll = lastYScroll;
-	}
-	public double getLastZoom() {
-		return lastZoom;
-	}
-	public void setLastZoom(double lastZoom) {
-		this.lastZoom = lastZoom;
-	}
-	public Date getLastUpdate() {
-		return lastUpdate;
-	}
-	public void setLastUpdate(Date lastUpdate) {
-		this.lastUpdate = lastUpdate;
-	}
-	public Date getLastCheck() {
-		return lastCheck;
-	}
-	public void setLastCheck(Date lastCheck) {
-		this.lastCheck = lastCheck;
-	}
-	public ArrayList<ImageModel> getImages() {
-		return images;
-	}
-	public void setImages(ArrayList<ImageModel> images) {
-		this.images = images;
-	}
-	public boolean isUpdatingFromInternet() {
-		return isUpdatingFromInternet;
-	}
-	public void setUpdatingFromInternet(boolean isUpdating) {
-		this.isUpdatingFromInternet = isUpdating;
-	}
-	public ArrayList<BookmarkModel> getBookmarks() {
-		if(this.bookmarks == null) {
-			try {
-				this.bookmarks = NovelsDao.getInstance().getBookmarks(getPageModel());
-			} catch (Exception e) {
-				Log.e(TAG, "Error when getting bookmarks: " + e.getMessage(), e);
-			}
-		}
-		return bookmarks;
-	}
-	public void setBookmarks(ArrayList<BookmarkModel> bookmarks) {
-		this.bookmarks = bookmarks;
-	}
+
+    public void setPageModel(PageModel pageModel) {
+        this.pageModel = pageModel;
+    }
+
+    public int getLastXScroll() {
+        return lastXScroll;
+    }
+
+    public void setLastXScroll(int lastXScroll) {
+        this.lastXScroll = lastXScroll;
+    }
+
+    public int getLastYScroll() {
+        return lastYScroll;
+    }
+
+    public void setLastYScroll(int lastYScroll) {
+        this.lastYScroll = lastYScroll;
+    }
+
+    public double getLastZoom() {
+        return lastZoom;
+    }
+
+    public void setLastZoom(double lastZoom) {
+        this.lastZoom = lastZoom;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Date getLastCheck() {
+        return lastCheck;
+    }
+
+    public void setLastCheck(Date lastCheck) {
+        this.lastCheck = lastCheck;
+    }
+
+    public ArrayList<ImageModel> getImages() {
+        return images;
+    }
+
+    public void setImages(ArrayList<ImageModel> images) {
+        this.images = images;
+    }
+
+    public boolean isUpdatingFromInternet() {
+        return isUpdatingFromInternet;
+    }
+
+    public void setUpdatingFromInternet(boolean isUpdating) {
+        this.isUpdatingFromInternet = isUpdating;
+    }
+
+    public ArrayList<BookmarkModel> getBookmarks() {
+        if (this.bookmarks == null) {
+            try {
+                this.bookmarks = NovelsDao.getInstance().getBookmarks(getPageModel());
+            } catch (Exception e) {
+                Log.e(TAG, "Error when getting bookmarks: " + e.getMessage(), e);
+            }
+        }
+        return bookmarks;
+    }
+
+    public void setBookmarks(ArrayList<BookmarkModel> bookmarks) {
+        this.bookmarks = bookmarks;
+    }
 }
